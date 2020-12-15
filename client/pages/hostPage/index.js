6fc3cd7ba9edb767de356db94280bb582d99de8c
Vikentii Ekgart <vikentii_ekgart@epam.com>
2020-12-15 15:46:43 +0400

    measure time before marquee ends

--- client/pages/hostPage/index.js
+++ client/pages/hostPage/index.js
@@ -3,8 +3,10 @@ const socket = io();
 const main = document.getElementsByClassName("main")[0];
 
 const TIMER = 12000;
+const SCROLLAMOUNT = 10;
+const MARQUEE_TIC = 85; //in ms default
 
-const msgArr = ['Мама, я в телевизоре (с) Викентий.'];
+const msgArr = ['Мама, я в телевизоре (с) Викентий. jh супер пупер длиное очень, даже да!'];
 
 
 socket.on('broadcast', message => {
@@ -16,10 +18,18 @@ const processMsg = (timeout) => {
     setTimeout(() => {
         main.innerHTML = '';
         const messageForPrint = msgArr.shift();
-        if (messageForPrint){
+        if (messageForPrint) {
+            
             const marqueeEl = buildMarquee(messageForPrint);
             main.innerHTML = marqueeEl;
-            processMsg(TIMER);
+            
+            const containerWidth = main.clientWidth;
+            const marquee = main.children[0];
+            const font = window.getComputedStyle(marquee).font;
+            const messageWidth = getTextWidth(messageForPrint, font);
+
+            const msToFinish = (containerWidth + messageWidth) / SCROLLAMOUNT * MARQUEE_TIC;
+            processMsg(msToFinish);
         } else {
             processMsg(500);
         }
@@ -31,3 +41,10 @@ const buildMarquee = (message) => {
 }
 
 processMsg(100);
+
+function getTextWidth(txt, font) {
+    this.element = document.createElement('canvas');
+    this.context = this.element.getContext("2d");
+    this.context.font = font;
+    return this.context.measureText(txt).width;
+}
